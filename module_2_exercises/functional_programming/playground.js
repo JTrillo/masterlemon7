4f5686c8d6dee8706a54835e197bd1fc8e1025a8
JTrillo <trillo@uma.es>
2019-11-20 00:17:35 +0100

    Updated functional programming playground

--- module_2_exercises/functional_programming/playground.js
+++ module_2_exercises/functional_programming/playground.js
@@ -9,24 +9,24 @@ const rectangles = [
 // MAP
 const getArea = ({ width, height }) => width * height;
 // Ejemplo:
-console.log(rectangles.map(getArea));
+console.log("MAP", rectangles.map(getArea));
 
 // FILTER
 // predicate == (x) => boolean
 const hasBigArea = (rectangle) => getArea(rectangle) > 50;
 // Ejemplo:
-console.log(rectangles.filter(hasBigArea));
+console.log("FILTER", rectangles.filter(hasBigArea));
 
 // SOME (llamada 'any' en algunos lenguajes funcionales)
 // Si algún elemento de la lista/array cumple el predicado devuelve un true. Si la lista/array es vacío devuelve false.
 // Ejemplo:
-console.log(rectangles.some(hasBigArea));
+console.log("SOME", rectangles.some(hasBigArea));
 
 // EVERY
 // Si todos los elementos de la lista/array cumplen el predicado devuelve un true. Si la lista/array es vacío devuelve true.
 const hasEvenArea = (element) => getArea(element) % 2 === 0;
 // Ejemplo:
-console.log(rectangles.every(hasEvenArea));
+console.log("EVERY", rectangles.every(hasEvenArea));
 
 // CONCAT
 const shape1 = { width: 11, height: 2 };
@@ -36,17 +36,39 @@ const otherShapes = [
     { width: 44, height: 5 }
 ];
 
-console.log(otherShapes, otherShapes.concat(shape1, shape2));
+console.log("CONCAT", otherShapes, otherShapes.concat(shape1, shape2));
 
 // REDUCE (llamada 'fold' en algunos lenguajes funcionales)
 // reducer === (acumulador, val) => acumulador
 const sumAllAreas = (total, rectangle) => total + getArea(rectangle);
 // Ejemplo:
-console.log(rectangles.reduce(sumAllAreas, 0)); //reduce(reducer, valorInicial)
+console.log("REDUCE", rectangles.reduce(sumAllAreas, 0)); //reduce(reducer, valorInicial)
 
 // REDUCERIGHT
 // Lo mismo que reduce pero empezando del último elemento al primero de la lista/array
 // Ejemplo:
 const oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(oneToTen.reduce((a, b) => a - b)); //-53
-console.log(oneToTen.reduceRight((a, b) => a - b)); //-35
+console.log("REDUCE vs", oneToTen.reduce((a, b) => a - b)); //-53
+console.log("REDUCE RIGHT", oneToTen.reduceRight((a, b) => a - b)); //-35
+
+
+// RECURSIVIDAD
+const recursiveSum1 = (arr) => {
+    if (!arr.length) return 0;
+    const [first, ...rest] = arr;
+    return first + recursiveSum1(rest);
+};
+const recursiveSum2 = (() => {
+    const innerRecursiveSum = (collection, index) => {
+        if (index < 0) return 0;
+        return collection[index] + innerRecursiveSum(collection, index - 1);
+    };
+    return (collection) => innerRecursiveSum(collection, collection.length - 1);
+})();
+// Ejemplo:
+console.log("SUMA RECURSIVA 1", recursiveSum1(oneToTen));
+console.log("SUMA RECURSIVA 2", recursiveSum2(oneToTen));
+
+
+// COMPOSICIÓN
+// PIPE --> reduce de funciones, COMPOSE --> reduceRight de funciones
