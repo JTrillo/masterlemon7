9ef3b1dd096a3ccfb8721d210d8788d448a87457
JTrillo <trillo@uma.es>
2019-11-22 20:48:04 +0100

    Module 2 ready to delivery

---
+++ module_2_lab/README.md
@@ -0,0 +1,30 @@
+To run each exercise you will have to run one of these commands:
+```
+# ONLY FIRST TIME
+npm install
+```
+
+* Exercise 1
+```
+npm run ex1
+```
+* Exercise 2
+```
+npm run ex2
+```
+* Exercise 3
+```
+npm run ex3
+```
+* Exercise 4
+```
+npm run ex4
+```
+* Exercise 4 with TypeScript
+```
+npm run ex4ts
+```
+* Exercise 5
+```
+npm run ex5
+```
--- module_2_lab/exercise1.js
+++ module_2_lab/exercise1.js
@@ -25,7 +25,8 @@ const last = (input) => {
 
 // Testing functions
 array = [2, 3, 4]
-console.log(head(array));
-console.log(tail(array));
-console.log(init(array));
-console.log(last(array));
+console.log("HEAD", head(array));
+console.log("TAIL", tail(array));
+console.log("INIT", init(array));
+console.log("LAST", last(array));
+console.log("ORIGINAL ARRAY", array); //Para comprobar que el array original no se ha modificado
