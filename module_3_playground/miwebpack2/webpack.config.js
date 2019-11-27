891c12888ecd4faf1699aed8c4e9c736f9007a4f
JTrillo <jokinator20@gmail.com>
2019-11-27 21:33:55 +0100

    Finished first session bundling

--- module_3_playground/miwebpack2/webpack.config.js
+++ module_3_playground/miwebpack2/webpack.config.js
@@ -1,13 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack"); //plugins por defecto de webpack
+
 module.exports = {
-    entry: ['regenerator-runtime/runtime', './students.js'],
+    entry: {
+        app: ["regenerator-runtime/runtime", "./students.js"],
+        appStyles: "./mystyles.css",
+        vendor: ["jquery"]
+    },
     output: {
-        filename: "bundle.js"
+        filename: "[name].[chunkhash].js" //incluye un hash al bundle para que siempre que se haga un build nuevo el nombre del fichero cambie para que el navegador lo tenga que volver a cargar
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }]
-    }
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 8084
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html", //este es el nombre que va a la carpeta dist
+            template: "index.html", //este es el fichero de origen
+            //hash: true, //incluye un hash al bundle para que siempre que se haga un build nuevo el nombre del fichero cambie para que el navegador lo tenga que volver a cargar
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
 };
