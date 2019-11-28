fc1beda29967a73ed087b8ab9b76717e3eadf583
JTrillo <jokinator20@gmail.com>
2019-11-28 14:16:36 +0100

    Bundling session 2 min. 28

--- module_3_playground/miwebpack2/webpack.config.js
+++ module_3_playground/miwebpack2/webpack.config.js
@@ -1,11 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require("webpack"); //plugins por defecto de webpack
 
 module.exports = {
     entry: {
         app: ["regenerator-runtime/runtime", "./students.js"],
         appStyles: "./mystyles.css",
-        vendor: ["jquery"]
+        //vendor: ["jquery"] //Esto ya no es necesario gracias a la seccion 'optimization', en la que incluimos todos los imports que se hacen en todos los .js nuestros (entre ellos, jquery)
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    chunks: 'all',
+                    name: 'vendor',
+                    test: /[\\/]node_modules[\\/]/,
+                    enforce: true,
+                },
+            },
+        },
     },
     output: {
         filename: "[name].[chunkhash].js" //incluye un hash al bundle para que siempre que se haga un build nuevo el nombre del fichero cambie para que el navegador lo tenga que volver a cargar
@@ -19,10 +32,12 @@ module.exports = {
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                use: [
-                    { loader: 'style-loader' },
+                /*use: [{ //loader: 'style-loader' //Este mete el css en js
+                        loader: MiniCssExtractPlugin.loader //Este genera ficheros css
+                    },
                     { loader: 'css-loader' }
-                ]
+                ]*/
+                use: [MiniCssExtractPlugin.loader, "css-loader"] //Si no tienen parámetros los loaders que usemos, podemos incluirlos en un array como este
             }
         ]
     },
@@ -38,6 +53,10 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[chunkhash].css",
+            chunkFilename: "[id].css"
         })
     ]
 };
