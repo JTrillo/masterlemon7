9b92787bb6414120937b8c4efc8aa6d8df336f0c
JTrillo <trillo@uma.es>
2019-11-30 18:17:27 +0100

    Bundling session 2 finished. All package-lock.json deleted

---
+++ module_3_playground/miwebpack2/src/students.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AverageComponent } from './averageComponent';
+const logoImg = require('./content/logo_1.png');
+
+ReactDOM.render(
+    <div>
+        <h1>Hello from React DOM</h1>
+        <AverageComponent />
+    </div>,
+    document.getElementById('root')
+);
+
+const img = document.createElement("img");
+img.src = logoImg;
+
+document.getElementById("imgContainer").appendChild(img);
--- module_3_playground/miwebpack2/webpack.config.js
+++ module_3_playground/miwebpack2/webpack.config.js
@@ -1,11 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require("webpack"); //plugins por defecto de webpack
+const path = require('path');
+
+const basePath = __dirname;
 
 module.exports = {
+    context: path.join(basePath, "src"),
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
     entry: {
-        app: ["regenerator-runtime/runtime", "./students.js"],
-        appStyles: "./mystyles.css",
+        app: ["regenerator-runtime/runtime", "./students.tsx"],
+        appStyles: "./mystyles.scss",
+        vendorStyles: ['../node_modules/bootstrap/dist/css/bootstrap.css'],
         //vendor: ["jquery"] //Esto ya no es necesario gracias a la seccion 'optimization', en la que incluimos todos los imports que se hacen en todos los .js nuestros (entre ellos, jquery)
     },
     optimization: {
@@ -23,23 +31,58 @@ module.exports = {
     output: {
         filename: "[name].[chunkhash].js" //incluye un hash al bundle para que siempre que se haga un build nuevo el nombre del fichero cambie para que el navegador lo tenga que volver a cargar
     },
+    devtool: "inline-source-map", //Para debugar .ts en Chrome
     module: {
         rules: [{
-                test: /\.js$/,
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                loader: 'awesome-typescript-loader',
+                options: {
+                    useBabel: true,
+                    "babelCore": "@babel/core", // needed for Babel v7
+                },
+            },
+            {
+                //test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                //exclude: /node_modules/, //Comentamos esto para que pueda funcionar bootstrap.css
                 /*use: [{ //loader: 'style-loader' //Este mete el css en js
                         loader: MiniCssExtractPlugin.loader //Este genera ficheros css
                     },
                     { loader: 'css-loader' }
                 ]*/
-                use: [MiniCssExtractPlugin.loader, "css-loader"] //Si no tienen parámetros los loaders que usemos, podemos incluirlos en un array como este
-            }
-        ]
+                use: [MiniCssExtractPlugin.loader, "css-loader"] //Si no tienen parámetros los loaders que usemos, podemos incluirlos en el array directamente
+            },
+            {
+                test: /\.(png|jpg)$/,
+                exclude: /node_modules/,
+                //loader: "file-loader",
+                loader: 'url-loader?limit=500' //si pesa menos de 500 bytes, mete la imagen en el bundle
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+        ],
     },
     devServer: {
         port: 8084
@@ -57,6 +100,6 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].[chunkhash].css",
             chunkFilename: "[id].css"
-        })
-    ]
+        }),
+    ],
 };
