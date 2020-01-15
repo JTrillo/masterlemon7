c03bdbcd84e6f6ee36f1ef8bf009d3a674c80e6b
JTrillo <jokinator20@gmail.com>
2020-01-15 13:45:06 +0100

    React class 4 completed

--- module_4_playground/react/first-project/src/pods/login/login.component.tsx
+++ module_4_playground/react/first-project/src/pods/login/login.component.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import TextField from "@material-ui/core/TextField";
+import { TextField } from "common/components/forms";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from '@material-ui/core/styles';
+import { LoginEntityVm } from './login.vm';
+import { Form, Field } from "react-final-form";
+import { formValidation } from "./login.validation";
 
 //Custom hook gracias a Material UI
 const useStyles = makeStyles({
@@ -15,23 +18,55 @@ const useStyles = makeStyles({
   }
 });
 
-interface Props {} //Hay que ponerlo por un bug de Material UI con Typescript
+interface Props {
+  onLogin: (loginInfo: LoginEntityVm) => void;
+  initialLogin: LoginEntityVm;
+} //Props hay que ponerlo por un bug de Material UI con Typescript
 
-export const LoginComponent: React.FunctionComponent = props => {
+export const LoginComponent: React.FunctionComponent<Props> = props => {
   const { formContainer } = useStyles(props);
+  const { onLogin, initialLogin } = props;
 
   return (
     <>
       <Card>
         <CardHeader title="Login" />
         <CardContent>
-          <div className={formContainer}>
-            <TextField label="Name" margin="normal" />
-            <TextField label="Password" type="password" margin="normal" />
-            <Button variant="contained" color="primary">
-              Login
-            </Button>
-          </div>
+          <Form
+            onSubmit={onLogin}
+            initialValues={initialLogin}
+            validate={formValidation.validateForm}
+            render={
+              ({ handleSubmit, submitting, pristine, values }) => (
+                <form onSubmit={handleSubmit}>
+                  <div className={formContainer}>
+                    <Field
+                      margin="normal"
+                      fullWidth
+                      name="login"
+                      component={TextField}
+                      type="text"
+                      label="Name"
+                    />
+                    <Field
+                      margin="normal"
+                      name="password"
+                      component={TextField}
+                      type="password"
+                      label="Password"
+                    />
+                    <Button type="submit" variant="contained" color="primary">
+                      Login
+                    </Button>
+                    <pre>{JSON.stringify(values, undefined, 2)}</pre>
+                    <Field name="login">
+                      {props => <pre>{JSON.stringify(props, undefined, 2)}</pre>}
+                    </Field>
+                  </div>
+                </form>
+              )
+            }
+          />
         </CardContent>
       </Card>
     </>
--- module_4_playground/react/first-project/src/pods/login/login.container.tsx
+++ module_4_playground/react/first-project/src/pods/login/login.container.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
 import { LoginComponent } from './login.component';
+import { useHistory } from 'react-router-dom';
+import { LoginEntityVm, createEmptyLogin } from './login.vm';
+import { linkRoutes } from 'core';
+import { validateCredentials } from './login.api';
 
 export const LoginContainer = () => {
-  return <LoginComponent />;
+  const history = useHistory();
+  const [initialLogin] = React.useState<LoginEntityVm>(createEmptyLogin());
+
+  const handleLogin = (loginInfo: LoginEntityVm) => {
+    validateCredentials(loginInfo.login, loginInfo.password).then(
+      areValidCredentials => {
+        areValidCredentials ? history.push(linkRoutes.hotelCollection) : alert('invalid credentials, use admin/test, excercise: display a mui snackbar instead of this alert.');
+      }
+    );
+  }
+
+  return <LoginComponent onLogin={handleLogin} initialLogin={initialLogin}/>;
 };
