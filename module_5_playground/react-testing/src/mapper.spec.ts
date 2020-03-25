1fc16575528eff8566ec5c3fb05102cc44943274
JTrillo <trillo@uma.es>
2020-03-26 00:59:40 +0100

    Testing class 1 completed

---
+++ module_5_playground/react-testing/src/mapper.spec.ts
@@ -0,0 +1,54 @@
+import * as apiModel from "./api-model";
+import * as viewModel from "./view-model";
+import { mapMemberListFromApiToVM } from "./mapper";
+
+describe('mapper specs', () => {
+  it('should return empty array when it feeds undefined', () => {
+    // Arrange
+    const members: apiModel.Member[] = undefined;
+
+    // Act
+    const result: viewModel.Member[] = mapMemberListFromApiToVM(members);
+
+    // Assert
+    expect(result).toEqual([]);
+  });
+
+  it('should return empty array when it feeds null', () => {
+    // Arrange
+    const members: apiModel.Member[] = null;
+
+    // Act
+    const result: viewModel.Member[] = mapMemberListFromApiToVM(members);
+
+    // Assert
+    expect(result).toEqual([]);
+  });
+
+  it('should return empty array when it feeds empty array', () => {
+    // Arrange
+    const members: apiModel.Member[] = [];
+
+    // Act
+    const result: viewModel.Member[] = mapMemberListFromApiToVM(members);
+
+    // Assert
+    expect(result).toEqual([]);
+  });
+
+  it('should return array one mapped item when it feeds array with one item', () => {
+    // Arrange
+    const members: apiModel.Member[] = [
+      { id: 1, login: 'test login', avatar_url: 'test avatar' },
+    ];
+
+    // Act
+    const result: viewModel.Member[] = mapMemberListFromApiToVM(members);
+
+    // Assert
+    const expectedResult: viewModel.Member[] = [
+      { id: '1', login: 'test login', avatarUrl: 'test avatar' },
+    ];
+    expect(result).toEqual(expectedResult);
+  });
+});
